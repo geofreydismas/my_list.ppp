cebee9bb77d717ce9f77703b0c79ebb687d2bf28
geofreydismas <geoffreydismas@hotmail.com>
2024-03-11 00:12:47 +0300

    first commit

---
+++ my_list.py
@@ -0,0 +1,23 @@
+# Initialize an empty list
+my_list = []
+
+# Append elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# Extend my_list with another list
+my_list.extend([50, 60, 70])
+
+# Remove the last element from my_list
+my_list.pop()
+
+# Sort my_list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+print(my_list.index(30))
